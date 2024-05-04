d08614eb9e9c756d5977c17137d1f329b8157acd
Richárd 36 <birorichard36@gmail.com>
2024-05-04 12:13:30 +0200

    Vizsgafeladatsor gyakorlas

---
+++ Programazos1VizsgaGyak/VizsgaGyak1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programazos1VizsgaGyak
+{
+    internal class VizsgaGyak1
+    {
+        public static void Run()
+        {
+            // 1. feladat
+            var numbers = new List<int>();
+
+            //numbers.Add(1);
+            //numbers.Add(3);
+            //numbers.Add(31);
+            //numbers.Add(42);
+
+            // 2. feladat
+            numbers.AddRange([1, 3, 31, 42, 50]);
+            numbers.AddRange([52, 63, 71, 82, 90]);
+
+            // 3. feladat
+            var numbersInDictionary = new Dictionary<int, string>();
+
+            // 4. feladat
+            foreach (var number in numbers) 
+            {
+                numbersInDictionary.Add(number, number.ToString());
+            }
+
+            // 5. feladat
+            foreach (var number in numbers)
+            {
+                if (number < 50)
+                {
+                    Console.WriteLine(number.ToString());
+                }
+            }
+
+        }
+    }
+}
---
+++ Programazos1VizsgaGyak/VizsgaGyak2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Programazos1VizsgaGyak
+{
+    internal class VizsgaGyak2
+    {
+        public static void Run()
+        {
+            // 1. feladat
+            var dogNames = new List<string>();
+
+            // 2. feladat
+            dogNames.AddRange(["Rex", "Bulldog", "Buksi", "Rexi", "Puppy"]);
+            dogNames.Sort();
+
+            // 3. feladat
+            // Ez is jó:
+            dogNames.ForEach(Console.WriteLine);
+
+            Console.WriteLine("\n");
+
+            // Ez is jó:
+            foreach (var dogName in dogNames)
+            {
+                Console.WriteLine(dogName);
+            }
+
+            // 4. feladat
+            var sunSystemPlanets = new Dictionary<string, int>();
+            sunSystemPlanets.Add("Merkúr", 0);
+            sunSystemPlanets.Add("Vénusz", 0);
+            sunSystemPlanets.Add("Föld", 1);
+            sunSystemPlanets.Add("Mars", 2);
+            sunSystemPlanets.Add("Jupiter", 95);
+            sunSystemPlanets.Add("Szaturnusz", 146);
+            sunSystemPlanets.Add("Uránusz", 28);
+            sunSystemPlanets.Add("Neptunusz", 16);
+
+            Console.WriteLine("\n");
+
+            foreach (var planetName in sunSystemPlanets.Keys)
+            {
+                int moonCount = sunSystemPlanets[planetName];
+
+                if (moonCount > 20) 
+                {
+                    Console.WriteLine(planetName);
+                }
+            }
+
+        }
+    }
+}
